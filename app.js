f206ec3db129eb49c3fbd78aae3539fffe3011cf
Evan Morikawa <evan@evanmorikawa.com>
2013-04-01 04:18:09 -0400

    Redirect from welcome

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/welcome', function(req, res){
+  res.redirect("https://docs.google.com/a/evanmorikawa.com/document/d/1Tnb163rvtWGYkJdrJ-6LbWpcZuDRVOMzpZ8EoQVD498/edit");
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
